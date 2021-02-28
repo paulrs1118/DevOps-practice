fb798ed2deaf56bb4d1dbcb94273b1b7fe1e687e
Paul Rivas <paulrs1118@gmail.com>
2021-02-28 22:53:15 +0000

    Add instance.tf file

---
+++ instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "terraform-ubuntu-instance" {
+  ami                         = "ami-03d315ad33b9d49c4"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.terraform_keypair.key_name
+  vpc_security_group_ids      = [aws_security_group.terraform-allow-ssh-sg.id]
+  subnet_id                   = aws_subnet.terraform_subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "terraform-Ubuntu"
+  }
+}
