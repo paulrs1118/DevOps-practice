ffa959f1399756d72d0dafc5d25f16e979b6f068
Paul Rivas <paulrs1118@gmail.com>
2021-03-01 19:40:58 +0000

    Changes on instance.tf file
    Adding a new instance on instance.tf file

--- instance.tf
+++ instance.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "terraform-ubuntu-instance" {
     Name = "terraform-Ubuntu"
   }
 }
+
+resource "aws_instance" "terraform-ubuntu-instance2" {
+  ami                         = "ami-03d315ad33b9d49c4"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.terraform_keypair.key_name
+  vpc_security_group_ids      = [aws_security_group.terraform-allow-ssh-sg.id]
+  subnet_id                   = aws_subnet.terraform_subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "terraform-Ubuntu2"
+  }
+}
