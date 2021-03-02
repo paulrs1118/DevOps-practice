04ddc83bfbdd80478970c3761fec711ece6542e5
Paul Rivas <paulrs1118@gmail.com>
2021-03-02 19:35:54 +0000

    Change on output.tf file

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "instance_public_ip" {
+output "instance1_public_ip" {
   value = aws_instance.terraform-ubuntu-instance.public_ip
+}
+output "instance2_public_ip"{
+ value =aws_instance.terraform-ubuntu-instance2.public_ip
 }
