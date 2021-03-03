915aa44874c21fa71522ebbc4d5646c9bf6715d2
Paul Rivas <paulrs1118@gmail.com>
2021-03-03 21:04:34 +0000

    Change on instance.tf file
    This change is about count on instance. tf file to create two instances
    at the same time

--- instance.tf
+++ instance.tf
@@ -1,25 +1,14 @@
 resource "aws_instance" "terraform-ubuntu-instance" {
+  count                       = 2
   ami                         = "ami-03d315ad33b9d49c4"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.terraform_keypair.key_name
-  vpc_security_group_ids      = [aws_security_group.terraform-allow-ssh-sg.id]
+  vpc_security_group_ids      = [aws_security_group.terraform-ssh-http-sg.id]
   subnet_id                   = aws_subnet.terraform_subnet.id
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.SSM_EC2_profile.name
 
   tags = {
     Name = "terraform-Ubuntu"
-  }
-}
-
-resource "aws_instance" "terraform-ubuntu-instance2" {
-  ami                         = "ami-03d315ad33b9d49c4"
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.terraform_keypair.key_name
-  vpc_security_group_ids      = [aws_security_group.terraform-allow-ssh-sg.id]
-  subnet_id                   = aws_subnet.terraform_subnet.id
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "terraform-Ubuntu2"
   }
 }
