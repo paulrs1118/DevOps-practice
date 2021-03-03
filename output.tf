8feb83f25d7dc174da303e7baf7c0c2fff2697b6
Paul Rivas <paulrs1118@gmail.com>
2021-03-03 21:06:31 +0000

    Change on output.tf file
    Changing on output.tf file to display the IP of the two new instances
    added with 'count'

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
-output "instance1_public_ip" {
-  value = aws_instance.terraform-ubuntu-instance.public_ip
-}
-output "instance2_public_ip"{
- value =aws_instance.terraform-ubuntu-instance2.public_ip
+output "instance_public_ip" {
+  value = aws_instance.terraform-ubuntu-instance.*.public_ip
 }
