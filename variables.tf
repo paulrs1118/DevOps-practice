00b70a8d149be7dc172ffa3ea9fd43847c1d95b3
Paul Rivas <paulrs1118@gmail.com>
2021-03-05 04:44:30 +0000

    Change on variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "aws_region" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" { sensitive = true }
+variable "aws_secret_key" { sensitive = true }
 variable "vpc_cidr" {}
 variable "cidr_block1" {}
+variable "domain" {}
