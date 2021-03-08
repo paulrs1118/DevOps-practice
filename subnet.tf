1f94c58aefb9b3444e08e3c352a1f3f1e9160138
Paul Rivas <paulrs1118@gmail.com>
2021-03-08 02:09:44 +0000

    change on subnet.tf file

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "terraform_subnet" {
   vpc_id                  = aws_vpc.terraform-vpc.id
   cidr_block              = var.cidr_block1
   map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
     Name = "terraform-subnet"
